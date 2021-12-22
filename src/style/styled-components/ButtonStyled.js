6cf562adb91a48d6acc1b94f8e43f127f4310000
Falcao1992 <eduardo.marselle@gmail.com>
2021-12-22 04:11:20 +0100

    create ButtonStyled.js

---
+++ src/style/styled-components/ButtonStyled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const ButtonStyled = styled.button`
+    background-color: #5e5af7;
+    color: #dcdbf9;
+    border: 0;
+    padding: 3px 7px;
+    border-radius: 8px;
+    cursor: pointer;
+    transition: all 0.3s linear;
+    &:hover {
+        background-color: #1b1a71;
+    }
+`
